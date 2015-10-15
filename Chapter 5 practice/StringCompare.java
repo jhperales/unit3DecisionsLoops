66536d606f18b2be023c3b717bcd0d7882f862a5
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-10-15 15:09:32 -0500

    update
    started StringCompare class

---
+++ Chapter 5 practice/StringCompare.java
@@ -0,0 +1,36 @@
+public class StringCompare
+{
+    public static void main(String [] args)
+    {
+        String word1 = "catalog";
+        String word2 = "cat";
+        
+        if (word1.compareTo("aaa") > 0)
+        {
+            System.out.println("word 1 is greater than aaa");
+        }
+        else
+        {
+            System.out.println("word 1 is not greater than aaa");
+        }
+        
+        if (word1.compareTo(word2) = 0)
+        {
+            System.out.println("word 1 and word 2 equals each other");
+        }
+        else
+        {
+            System.out.println("word 1 and word 2 do not equal each other");
+        }
+        
+        if (word1.comepareTo(word2) < 0)
+        {
+            System.out.println("word 1 is less than word 2");
+        }
+        else
+        {
+            System.out.println("word 1 is not less than word 2");
+        }
+        
+        String word1_three = word1.substring(0
+            
