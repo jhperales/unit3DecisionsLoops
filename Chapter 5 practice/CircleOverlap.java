7a2c2349ef1582e6627242c81893eb47e1cdf58c
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-10-19 15:07:04 -0500

    update
    started circle lap

---
+++ Chapter 5 practice/CircleOverlap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CircleOverlap
+{
+   public static void main(String[] args)
+   {
+      Scanner s = new Scanner(System.in);
+
+      System.out.print("Input the radius of the first circle: ");
+      double radius1 = s.nextDouble(); 
+      double xcenter1 = 0;
+      double ycenter1 = 0;
+      System.out.print("Input the radius of the second circle: ");
+      double radius2 = s.nextDouble(); 
+      double xcenter2 = 40;
+      double ycenter2 = 0;
+
+      System.out.println("im done. do this yourself by hand");
+   }
+}
