7720802e41e1204c471f2268208eb5d0730d9797
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-10-22 15:04:28 -0500

    update
    finished spectrum class, begin magpie

--- Chapter 5 practice/Spectrum.java
+++ Chapter 5 practice/Spectrum.java
@@ -8,9 +8,37 @@ public class Spectrum
         System.out.println("Enter a wavelength in scientific notation/epsilon value ((Ex: 1e-12)): ");
         double wavelength = s.nextDouble();
         
-        if (wavelength > 1e-1)
+        if (wavelength < 1e-11)
         {
-            System.out.println("The type of wave is Radio Waves");
+            System.out.println("Type: Gamma Rays\n Frequency = > 3e19");
+        }
+        else if (wavelength < 1e-8)
+        {
+            System.out.println("Type: X-Rays\n Frequency = 3e16 to 3e19");
+        }
+        else if (wavelength < 4e-7)
+        {
+            System.out.println("Type: Ultraviolet\n Frequency = 7.5e14 to 3e16");
+        }
+        else if (wavelength < 7e-7)
+        {
+            System.out.println("Type: Visible Light\n Frequency = 4e14 to 7.5e14");
+        }
+        else if (wavelength < 1e-3)
+        {
+            System.out.println("Type: Infrared\n Frequency = 3e11 to 4e14");
+        }
+        else if (wavelength < 1e-1)
+        {
+            System.out.println("Type: Microwaves\n Frequency = 3e9 to 3e11");
+        }
+        else if (wavelength > 1e-1)
+        {
+            System.out.println("Type: Radio Waves\n Frequency = less than 3e9");
+        }
+        else
+        {
+            System.out.println("Unable to determain");
         }
     }
 }
