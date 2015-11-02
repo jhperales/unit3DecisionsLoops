f5185453de843deaaf90529226c83b85f26564b1
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-11-02 15:05:34 -0600

    update
    worked on diamond more

--- Capter 6 practice/Diamond.java
+++ Capter 6 practice/Diamond.java
@@ -1,3 +1,23 @@
 import java.util.Scanner;
 
 public class Diamond
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        
+        int input = s.nextInt();
+        
+        String derp = "         *";
+        
+        for (int i = 0;
+             i < input*2;
+             i++)
+             {
+                 System.out.println(derp);
+                 derp += "**";
+             }
+    }
+}
+                 
