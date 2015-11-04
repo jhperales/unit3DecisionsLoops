e43a528cac9874c1b1adce78e028f3588043b077
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-11-04 14:57:36 -0600

    dftgears``

--- Capter 6 practice/fibonacci_python.py
+++ Capter 6 practice/fibonacci_python.py
@@ -6,7 +6,16 @@ f2_input = int(raw_input("Enter another integer: "))
 def algorithm(f1, f2):
     f1 = f1
     f2 = f2
-    fnew = f1 + f2
+
+    for i in range(f1_input):
+        f2 = f1
+        f1 = fnew
+        fnew = f1 + f2
+        print fnew
+
+algorithm(f1_input, f2_input)
+
+    
     
 
     
